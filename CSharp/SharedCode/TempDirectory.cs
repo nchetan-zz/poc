bddaca92919dcd0ba02497bc0cef5c3fbd89b187
Chetan Nadgouda <chetann@isoftstone.com>
2015-07-21 15:38:36 -0700

    Add SharedCode project. Implement TempDirectory Disposable.

---
+++ CSharp/SharedCode/TempDirectory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace SharedCode
+{
+    public class TempDirectory : IDisposable
+    {
+        public string FullPath { get; private set; }
+
+        public TempDirectory()
+        {
+            var guid = Guid.NewGuid();
+            var directoryName = guid.ToString("N");
+            var tempPath = Path.GetTempPath();
+            FullPath = Path.Combine(tempPath, directoryName);
+
+            Directory.CreateDirectory(FullPath);
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(FullPath, true);
+        }
+    }
+}
