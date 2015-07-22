5a51e228992ea0d3368be77e99f3d4bce88ff1f9
Chetan Nadgouda <chetann@isoftstone.com>
2015-07-22 15:30:57 -0700

    Create RedisCache application

---
+++ CSharp/RedisCaching/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisCaching
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var connection = ConnectionMultiplexer.Connect("localhost,ssl=true");
+
+            }
+            catch (Exception ex)
+            {
+                var oldForgroundColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error ");
+
+            }
+
+        }
+
+        
+    }
+}
