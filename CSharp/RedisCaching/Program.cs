87b80292ad62874967418cc171ed78b641f6715f
Chetan Nadgouda <chetann@isoftstone.com>
2015-07-22 17:00:33 -0700

    WIP: Redis cache AppendToCacheValue demonstration

--- CSharp/RedisCaching/Program.cs
+++ CSharp/RedisCaching/Program.cs
@@ -1,31 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using StackExchange.Redis;
 
 namespace RedisCaching
 {
     static class Program
     {
+        private const string CacheKey = "Some Key";
+        private const string CacheValue = "Some value for redis cache";
+
         static void Main(string[] args)
         {
             try
             {
-                var connection = ConnectionMultiplexer.Connect("localhost,ssl=true");
+                ConnectionMultiplexer connection = ConnectionMultiplexer.Connect("localhost");
+                IDatabase database = connection.GetDatabase();
 
+                _SetAndGetCacheValue(database);
+                _AppendToCacheValue(database);
             }
             catch (Exception ex)
             {
-                var oldForgroundColor = Console.ForegroundColor;
+                ConsoleColor oldForgroundColor = Console.ForegroundColor;
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Error ");
-
+                Console.WriteLine($"Error {ex.Message}");
             }
-
         }
 
-        
+        private static void _AppendToCacheValue(IDatabase database)
+        {
+            const string appendString = " appended";
+            database.StringAppend(CacheValue, appendString);
+            var retrievedValue = database.StringGet(CacheKey);
+            var expected = $"{CacheKey}{appendString}";
+            Console.WriteLine($"AppendToCacheValue: Expected = {expected}, Actual = {retrievedValue}");
+        }
+
+        private static void _SetAndGetCacheValue(IDatabase database)
+        {
+            database.StringSet(CacheKey, CacheValue);
+
+            RedisValue retrievedValue = database.StringGet(CacheKey);
+            Console.WriteLine($"Expected value = {CacheValue}, Retrieved value = {retrievedValue}");
+        }
     }
 }
