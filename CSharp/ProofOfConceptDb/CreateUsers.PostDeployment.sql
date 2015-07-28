62bfc24a42c8696a8360a56311a6f3e4c79b0647
Chetan Nadgouda <chetann@isoftstone.com>
2015-07-28 11:08:49 -0700

    Proof of Concept DB project created.

---
+++ CSharp/ProofOfConceptDb/CreateUsers.PostDeployment.sql
@@ -0,0 +1,15 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+INSERT INTO [User] (Id, UserName) VALUES (1, 'chetan')
+INSERT INTO [User] (Id, UserName) VALUES (2, 'user2')
+INSERT INTO [User] (Id, UserName) VALUES (3, 'user3')
---
+++ CSharp/ProofOfConceptDb/User.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL, 
+    [UserName] NVARCHAR(100) NOT NULL, 
+    CONSTRAINT [PK_User] PRIMARY KEY ([Id]), 
+    CONSTRAINT [UQ_User_Column] UNIQUE ([UserName]),
+)
