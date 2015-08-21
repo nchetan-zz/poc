69666952666d45136651d647bf4de72a848aaea8
Chetan Nadgouda <chetann@isoftstone.com>
2015-08-21 10:14:12 -0700

    Fix UpdateTextViewInMainActivity code.

--- Android/UpdateTextViewInMainActivity/app/src/main/java/com/pradhee/chetann/updatetextviewinmainactivity/MainActivity.java
+++ Android/UpdateTextViewInMainActivity/app/src/main/java/com/pradhee/chetann/updatetextviewinmainactivity/MainActivity.java
@@ -20,9 +20,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+
         mTextMessageTextView = (TextView) findViewById(R.id.textMessage);
         mTextMessageTextView.setText("Hellooooo");
-        setContentView(R.layout.activity_main);
     }
 
     @Override
