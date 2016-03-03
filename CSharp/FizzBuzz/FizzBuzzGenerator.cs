ef62d5dbe3303de58232217acaea50907d0576fb
Chetan Nadgouda <chetan@pradhee.com>
2016-03-03 10:24:26 -0800

    FizzBuzz implementation for Dev9

---
+++ CSharp/FizzBuzz/FizzBuzzGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzGenerator
+    {
+        public string Execute()
+        {
+            const int firstMultiple = 3;
+            const int secondMultiple = 5;
+            const int multiple = firstMultiple*secondMultiple;
+
+            var resultBuilder = new StringBuilder();
+            for (var currentNumber = 1; currentNumber <= 15; currentNumber++)
+            {
+                if (currentNumber%multiple == 0)
+                {
+                    resultBuilder.AppendLine("FizzBuzz");
+                    continue;
+                }
+
+                if (currentNumber%3 == 0)
+                {
+                    resultBuilder.AppendLine("Fizz");
+                    continue;
+                }
+
+                if (currentNumber%5 == 0)
+                {
+                    resultBuilder.AppendLine("Buzz");
+                    continue;
+                }
+
+                resultBuilder.AppendLine($"{currentNumber}");
+            }
+
+            return resultBuilder.ToString();
+        }
+    }
+}
---
+++ CSharp/FizzBuzzUnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FizzBuzz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expected = @"1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+";
+
+            var serviceUnderTest = new FizzBuzzGenerator();
+            var actual = serviceUnderTest.Execute();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
